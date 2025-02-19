ed6b6b1a492d2b0fbda68d897822834f2fe48fd3
wq2581 <49905511+wq2581@users.noreply.github.com>
2025-02-19 09:52:48 -0500

    Update utils.py

--- code/DS-qwen/utils.py
+++ code/DS-qwen/utils.py
@@ -1,27 +1,4 @@
-class AvgMeter:
-    def __init__(self, name="Metric"):
-        self.name = name
-        self.reset()
 
-    def reset(self):
-        self.avg, self.sum, self.count = [0] * 3
-
-    def update(self, val, count=1):
-        self.count += count
-        self.sum += val * count
-        self.avg = self.sum / self.count
-
-    def __repr__(self):
-        text = f"{self.name}: {self.avg:.4f}"
-        return text
-
-def get_lr(optimizer):
-    for param_group in optimizer.param_groups:
-        return param_group["lr"]
-
-
-from sklearn.metrics import accuracy_score
-import torch
 
 
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, precision_score, recall_score
